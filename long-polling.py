37f6cfa8a7c52faac10c1fbc8ea9b01be588bf94
AkshitBhandariCodes <akshitbhandaricodes@gmail.com>
2025-02-23 00:19:20 +0530

    Added new command: Tell me a Joke -> by AkshitBhandariCodes - 27016]
    Added a New Functionality tell me a Joke using Python with API calling.

--- long-polling.py
+++ long-polling.py
@@ -28,6 +28,21 @@ def send_message(chat_id, text):
     data = {"chat_id": chat_id, "text": text}
     requests.post(url, data=data)
 
+
+"""
+Added by AkshitBhandari - 27016
+This function uses and API to fetch an joke from the joke API 
+It basically provides us with a python dictionary that has keys like type, setup and punchline which contains specific string (or we can say the main content or joke)
+This data will be called to show up the joke as I did in line 43 of code
+"""
+def get_joke():
+    joke_url = "https://official-joke-api.appspot.com/jokes/random"
+    response = requests.get(joke_url)
+    if response.status_code == 200:
+        joke_data = response.json()
+        return f"{joke_data['setup']}\n{joke_data['punchline']}"
+    return "Sorry, I couldn't fetch a joke at the moment."
+
 def main():
     update_id = None
     print("Bot started...")
@@ -37,11 +52,18 @@ def main():
             update_id = update["update_id"] + 1
             message = update.get("message")
             chat_id = message.get("chat", {}).get("id", None)
-
+            text = message.get("text", "").strip().lower()
             # Add your command in this block by using elif
             if message and message.get("text", "").strip() == "/start":
                 greeting = random.choice(greetings)
                 send_message(chat_id, greeting)
+            elif text == "/joke":
+                """
+                Added by AkshitBhandari - 27016
+                This block check if the command /joke is typed by the user while using the bot and helps us to send the message or joke (refer line 66)
+                """
+                joke = get_joke()
+                send_message(chat_id, joke)
             else:
                 send_message(chat_id, "Invalid message")
 
