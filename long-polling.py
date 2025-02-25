efefbfb1e2f424e3d73ba96a5506653d10313e03
Adarsh Kumar <adarshkr357@gmail.com>
2025-02-25 21:23:07 +0530

    Added check condition on BOT_TOKEN

--- long-polling.py
+++ long-polling.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found. Please set it in .env file.")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
 
 greetings = [
